84c808093c1182b3688edb9fe44f91bcf343bafa
wlzhaofeng <wlzhaofeng@126.com>
2018-11-07 09:21:58 +0800

    zhaofeng

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 //使用express构建web服务器 --11:25
 const express = require('express');
-const bodyParser = require('body-parser');
 var app = express();
 var server = app.listen(3000);
-//使用body-parser中间件
-app.use(bodyParser.urlencoded({extended:false}));
 //托管静态资源到public目录下
 app.use(express.static('public'));
 
