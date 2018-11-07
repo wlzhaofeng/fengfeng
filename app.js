bc7c5e8cf417a4aad65fb2de78a85d59641fe7c3
wlzhaofeng <wlzhaofeng@126.com>
2018-11-07 08:54:56 +0800

    zhaofeng

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 //使用express构建web服务器 --11:25
 const express = require('express');
 const bodyParser = require('body-parser');
-/*引入路由模块*/
-const index=require("./routes/index");
 var app = express();
 var server = app.listen(3000);
 //使用body-parser中间件
 app.use(bodyParser.urlencoded({extended:false}));
 //托管静态资源到public目录下
 app.use(express.static('public'));
-/*使用路由器来管理路由*/
-app.use("/index",index);
+
 
 
